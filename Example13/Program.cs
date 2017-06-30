2570852c01feb58d3d187ddaac0313610b7a87fd
Chuck Lu <244657538@qq.com>
2017-06-30 20:09:24 +0800

    add exception handler for Example 13

--- Example13/Program.cs
+++ Example13/Program.cs
@@ -10,7 +10,7 @@ namespace Example13
         {
             Console.WriteLine("List all");
 
-            using(var connection = new MySqlConnection
+            using (var connection = new MySqlConnection
             {
                 ConnectionString = "server=127.0.0.1;user id=root;password=123456;persistsecurityinfo=True;database=School;"
             })
@@ -18,22 +18,22 @@ namespace Example13
                 connection.Open();
 
                 var command = new MySqlCommand("SELECT FirstName, LastName FROM Students", connection);
- 
-                using (var reader =  command.ExecuteReader())
+
+                using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
                         Console.WriteLine($"{reader["FirstName"]}\t\t{reader["LastName"]}");
                     }
                 }
-    
+
                 connection.Close();
             }
         }
 
         static void AddStudent(string firstName, string lastName, int englishScore, int mathScore)
         {
-            using(var connection = new MySqlConnection
+            using (var connection = new MySqlConnection
             {
                 ConnectionString = "server=127.0.0.1;user id=root;password=123456;persistsecurityinfo=True;database=School;"
             })
@@ -46,22 +46,33 @@ namespace Example13
                 command.Parameters.AddWithValue("@LastName", lastName);
                 command.Parameters.AddWithValue("@EnglishScore", englishScore);
                 command.Parameters.AddWithValue("@MathScore", mathScore);
- 
+
                 command.ExecuteNonQuery();
-    
+
                 connection.Close();
             }
         }
 
         static void Main(string[] args)
         {
-            List();
-            
-            AddStudent("Debao", "Wang", 88, 99);
-            AddStudent("Philips", "Zhang", 77, 98);
+            try
+            {
+                List();
 
-            List();
+                AddStudent("Debao", "Wang", 88, 99);
+                AddStudent("Philips", "Zhang", 77, 98);
 
+                List();
+            }
+            catch (Exception ex)
+            {
+                while (ex != null)
+                {
+                    Console.WriteLine("ErrorMessage:{0}{1}ExceptionType:{2}{1}Assembly:{3}{1}{1}Stack Trace:{1}{4}{1}",
+                        ex.Message, Environment.NewLine, ex.GetType(), ex.Source, ex.StackTrace);
+                    ex = ex.InnerException;
+                }
+            }
             Console.ReadKey();
         }
     }
